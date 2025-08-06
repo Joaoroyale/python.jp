71991fa20211eacbc8c93ceb498a7bfddd5b6f38
João Pedro Mendonça Joaquim da Silva <jpmjs@palmares.ifpe.edu.br>
2025-08-06 09:20:07 -0300

    aula,06/08

--- python.jp/jp.py
+++ python.jp/jp.py
@@ -1,1 +1,6 @@
-print ("Se quiser soma, digite: A. Se quiser a subtração digite: B. se quiser subtração digite: C. Se quiser multiplicação digite: D ")
+n1 = float(input("Qual o pirmeiro numero:"))
+n2 = float(input("Qual o segundo numero:"))
+resultado = n1 + n2
+lista = []
+lista.append (f"{resultado}")
+print (lista)
