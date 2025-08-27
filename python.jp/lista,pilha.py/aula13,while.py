bac4c2f11e5f12aae34dd2e2c52cecf89cb5fafd
João Pedro Mendonça Joaquim da Silva <jpmjs@palmares.ifpe.edu.br>
2025-08-27 09:43:26 -0300

    João is macho

---
+++ python.jp/lista,pilha.py/aula13,while.py
@@ -0,0 +1,12 @@
+pilha = []
+x = 0
+while x < 5:
+    palavra = input("Qual palavra vocé quer ?:")
+    pilha.append (f"{palavra}")
+    print (pilha)
+    x = x + 1
+y = 0
+while y < 5:
+    pilha.pop ()
+    print (pilha)
+    y = y + 1
